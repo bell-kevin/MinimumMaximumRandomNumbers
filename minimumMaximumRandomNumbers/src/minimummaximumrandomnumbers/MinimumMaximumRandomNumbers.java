7d2682d4dd7a316328d609f0dbef9331835bc583
Kevin Bell <bellKevin@pm.me>
2022-05-20 02:32:47 +0000

    Update MinimumMaximumRandomNumbers.java

--- minimumMaximumRandomNumbers/src/minimummaximumrandomnumbers/MinimumMaximumRandomNumbers.java
+++ minimumMaximumRandomNumbers/src/minimummaximumrandomnumbers/MinimumMaximumRandomNumbers.java
@@ -5,6 +5,9 @@
  */
 package minimummaximumrandomnumbers;
 
+import java.util.Scanner;
+import java.util.Random;
+
 /**
  *
  * @author 4800590195
@@ -16,6 +19,18 @@ public class MinimumMaximumRandomNumbers {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.print("Random Number in a Range by Kevin Bell \n");
+        Scanner computerKeyboardInput = new Scanner(System.in);
+        Random rng = new Random();
+        System.out.print("Please enter a minimum value: ");
+        String minimumInputString = computerKeyboardInput.next();
+        int minimumInput = Integer.parseInt(minimumInputString);
+        System.out.print("Please enter a maximum value: ");
+        String maximumInputString = computerKeyboardInput.next();
+        int maximumInput = Integer.parseInt(maximumInputString);
+        int randomNumber = (int) ((maximumInput - minimumInput + 1) * Math.random() + minimumInput);
+        int randomClassNumber = rng.nextInt((maximumInput - minimumInput + 1) + minimumInput);
+        System.out.println("Random number between 3 and 17 is " + randomNumber);
+        System.out.println("Random number using Random class: " + randomClassNumber);
     }
-    
 }
