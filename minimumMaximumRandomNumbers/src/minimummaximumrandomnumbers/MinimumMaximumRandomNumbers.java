8e0b7e5772e0816b0663f403a53541b0c436ae8b
Kevin Bell <bellKevin@pm.me>
2022-05-23 15:05:41 +0000

    Update MinimumMaximumRandomNumbers.java

--- minimumMaximumRandomNumbers/src/minimummaximumrandomnumbers/MinimumMaximumRandomNumbers.java
+++ minimumMaximumRandomNumbers/src/minimummaximumrandomnumbers/MinimumMaximumRandomNumbers.java
@@ -9,23 +9,23 @@ public class MinimumMaximumRandomNumbers {
         System.out.print("Random Number in a Range by Kevin Bell \n");
         Scanner computerKeyboardInput = new Scanner(System.in);
         Random rng = new Random();
-        System.out.print("Please enter a minimum value: ");
-        String minimumInputString = computerKeyboardInput.next();
-        int minimumInput = Integer.parseInt(minimumInputString);
-        System.out.print("Please enter a maximum value: ");
-        String maximumInputString = computerKeyboardInput.next();
-        int maximumInput = Integer.parseInt(maximumInputString);
+        System.out.print("Please enter a minimum value: "); //prompt user input
+        String minimumInputString = computerKeyboardInput.next(); //get user input
+        int minimumInput = Integer.parseInt(minimumInputString); //parse user input string into integer
+        System.out.print("Please enter a maximum value: "); //prompt user input
+        String maximumInputString = computerKeyboardInput.next(); //get user input
+        int maximumInput = Integer.parseInt(maximumInputString); //parse user input string into integer
         while (minimumInput > maximumInput) {
-            System.out.print("Please enter a minimum value: ");
-            minimumInputString = computerKeyboardInput.next();
-            minimumInput = Integer.parseInt(minimumInputString);
-            System.out.print("Please enter a maximum value: ");
-            maximumInputString = computerKeyboardInput.next();
-            maximumInput = Integer.parseInt(maximumInputString);
+            System.out.print("Please enter a minimum value: "); //prompt user input
+            minimumInputString = computerKeyboardInput.next(); //get user input
+            minimumInput = Integer.parseInt(minimumInputString); //parse user input string into integer
+            System.out.print("Please enter a maximum value: "); //prompt user input
+            maximumInputString = computerKeyboardInput.next(); //get user input
+            maximumInput = Integer.parseInt(maximumInputString); //parse user input string into integer
         }
         int randomNumber = (int) ((maximumInput - minimumInput + 1) * Math.random() + minimumInput);
         int randomClassNumber = rng.nextInt(maximumInput - minimumInput + 1) + minimumInput;
-        System.out.println("Random number between 3 and 17 is " + randomNumber);
-        System.out.println("Random number using Random class: " + randomClassNumber);
+        System.out.println("Random number between " + minimumInput + " and " + maximumInput + " is " + randomNumber); //output
+        System.out.println("Random number using Random class: " + randomClassNumber); //output
     }
 }
